2ebfbf50d7180d2e97d027cc9ba9ec392932654e
kobbin003 <dkfeto@gmail.com>
2023-04-07 12:43:47 +0530

    uri update

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,24 +1,23 @@
 import mongoose from "mongoose";
-import { ServerApiVersion } from "mongodb";
+import { ServerApiVersion, MongoClient } from "mongodb";
 import validator from "validator";
 import "dotenv/config";
 // the uri
-const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
-const uri = `mongodb+srv://${username}:${password}@cluster0.ibgdcyq.mongodb.net/?retryWrites=true&w=majority`;
-
+// console.log("username", process.env.USERNAME, process.env.PASS);
+const uri = `mongodb+srv://kobin:${password}@cluster0.ibgdcyq.mongodb.net/?retryWrites=true&w=majority`;
 // connect to mongoose
 async function connectDb() {
-  try {
-    mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverApi: ServerApiVersion.v1,
-    });
-    console.log("db connected!");
-  } catch (error) {
-    console.log("db connection could not be established!", error);
-  }
+	try {
+		mongoose.connect(uri, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			serverApi: ServerApiVersion.v1,
+		});
+		console.log("db connected!");
+	} catch (error) {
+		console.log("db connection could not be established!", error);
+	}
 }
 
 export default connectDb;
